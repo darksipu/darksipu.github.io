5b9416ee0ea626e981e7cc749db6e8b673dcff60
florin <florin.gheorghiu24@gmail.com>
2017-11-28 23:08:55 +0000

    second draft

--- actions.js
+++ actions.js
@@ -5,6 +5,19 @@
 
 $( document ).ready(function() {
 
+    $("#about").click(function(){
+
+        $("#pictures").children().hide();
+        $("#descriptions").children().hide();
+
+        $("#pictures-intro").show();
+        $("#introbit").fadeIn('slow');
+
+
+    });
+
+
+//---------------------------------------3rd year section button actions
     $("#3rd").click(function(){
 
         $("#pictures").children().hide();
@@ -13,19 +26,32 @@ $( document ).ready(function() {
         $("#pictures-3rd").fadeIn();
         $("#descriptions-3rd").fadeIn('slow');
 
-        $("#descriptions-3rd").children().slice(0,5).hide();
-        $("#descriptions-3rd").children().slice(0,4).fadeIn(2300);
+        $("#descriptions-3rd").children().slice(0,12).hide();
+        $("#descriptions-3rd").children().slice(0,3).fadeIn(2300);
 
     });
 
     //activates the first button
-    $("#button-3rd").click(function(){
-        $("#descriptions-3rd").children().slice(0,4).hide();
+    $("#button-3rd1").click(function(){
+        $("#descriptions-3rd").children().slice(0,12).hide();
 
-        $("#descriptions-3rd").children().slice(4,5).fadeIn(1500);
+        $("#descriptions-3rd").children().slice(3,6).fadeIn(1500);
 
     });
 
+    $("#button-3rd2").click(function(){
+        $("#descriptions-3rd").children().slice(0,12).hide();
+
+        $("#descriptions-3rd").children().slice(6,9).fadeIn(1500);
+
+    });
+
+    $("#button-3rd3").click(function(){
+        $("#descriptions-3rd").children().slice(0,12).hide();
+
+        $("#descriptions-3rd").children().slice(9,12).fadeIn(1500);
+
+    });
 
 //---------------------------------------ML section button actions
     $("#4th").click(function(){
@@ -61,7 +87,7 @@ $( document ).ready(function() {
         $("#pictures-games").fadeIn(1900);
         $("#descriptions-games").fadeIn(2300);
 
-        $("#descriptions-games").children().slice(0,12).hide();
+        $("#descriptions-games").children().slice(0,13).hide();
         $("#descriptions-games").children().slice(0,3).fadeIn(2300);
 
 
@@ -69,7 +95,7 @@ $( document ).ready(function() {
 
     //activates the first button
     $("#button-games1").click(function(){
-        $("#descriptions-games").children().slice(0,12).hide();
+        $("#descriptions-games").children().slice(0,13).hide();
 
         $("#descriptions-games").children().slice(3,6).fadeIn(1500);
 
@@ -77,7 +103,7 @@ $( document ).ready(function() {
 
     //activates the second button
     $("#button-games2").click(function(){
-        $("#descriptions-games").children().slice(0,12).hide();
+        $("#descriptions-games").children().slice(0,13).hide();
 
         $("#descriptions-games").children().slice(6,9).fadeIn(1500);
     });
@@ -85,9 +111,9 @@ $( document ).ready(function() {
     //activates the third button
     $("#button-games3").click(function(){
 
-        $("#descriptions-games").children().slice(0,12).hide();
+        $("#descriptions-games").children().slice(0,13).hide();
 
-        $("#descriptions-games").children().slice(9,12).fadeIn(1500);
+        $("#descriptions-games").children().slice(9,13).fadeIn(1500);
     });
 
 
@@ -100,30 +126,47 @@ $( document ).ready(function() {
         $("#pictures-website").fadeIn();
         $("#descriptions-website").fadeIn(2300);
 
-        $("#descriptions-website").children().slice(0,2).hide();
-        $("#descriptions-website").children().slice(0,2).fadeIn(2300);
+        $("#descriptions-website").children().slice(0,10).hide();
+        $("#descriptions-website").children().slice(0,3).fadeIn(2300);
 
     });
-
 
     //activates the first button
-    $("#button-website").click(function(){
-        $("#descriptions-website").children().slice(0,2).hide();
+    $("#button-website1").click(function(){
+        $("#descriptions-website").children().slice(0,10).hide();
 
-        $("#descriptions-website").children().slice(2,3).fadeIn(1500);
+        $("#descriptions-website").children().slice(3,6).fadeIn(1500);
 
     });
 
+    //activates the second button
+    $("#button-website2").click(function(){
+        $("#descriptions-website").children().slice(0,10).hide();
 
-//---------------------------------------website section button actions
-    $("#swarms").click(function(){
+        $("#descriptions-website").children().slice(6,8).fadeIn(1500);
+
+    });
+
+    //activates the third button
+    $("#button-website3").click(function(){
+        $("#descriptions-website").children().slice(0,10).hide();
+
+        $("#descriptions-website").children().slice(8,10).fadeIn(1500);
+
+    });
+
+//---------------------------------------HATCH section button actions
+    $("#hatch").click(function(){
 
         $("#pictures").children().hide();
         $("#descriptions").children().hide();
 
-        $("#description-3rd").fadeIn('slow');
+        $("#description-hatch").fadeIn('slow');
 
     });
+
+
+//---------------------------------------CONTACT
 
     $("#contact").click(function() {
 
